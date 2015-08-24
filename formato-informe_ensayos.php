bc014136bf9072023c9d61f6505eb60b9437e73d
jluisj7 <jluisjab7@gmail.com>
2015-08-24 02:56:57 -0500

    formato informe

---
+++ formato-informe_ensayos.php
@@ -0,0 +1,286 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Formato Cotizacion</title>
+	<style>
+	*{
+		font-size: 12px;
+	}
+	table{
+		border: 0;
+	}
+
+	tr{
+		border:0;
+	}
+	td{
+		border-left: 1px solid #000;
+   	 border-bottom: 1px solid;
+   	     padding-left: 6px;
+	}
+	.bold{
+		font-weight: bold;
+	}
+	
+
+	.b_t{
+		border-top: 1px solid #000;
+	}
+	.b_r{
+	border-right: 1px solid #000;
+	}
+	.b_l{
+	border-left: 1px solid #000;
+	}
+	.b_b{
+	border-bottom: 1px solid #000;
+	}
+
+	.n_b_b{
+	border-bottom: 0px;
+	}
+	.n_b_l{
+	border-left: 0px;
+	}
+	.n_b_r{
+	border-right: 0px;
+	}
+	.n_b_t{
+	border-top: 0px;
+	}
+
+	.det{
+		padding: 8px;
+		text-align: center;
+		font-weight: bold;
+		text-decoration: underline;
+	}
+	.align-c{
+		text-align: center;
+	}
+	.align-r{
+		text-align: right;
+	}
+
+	.rtc{
+		font-weight: bold;
+	}
+
+	.firma p{
+		margin: 0;
+		font-weight: bold;
+		}
+		hr{
+		margin-left: 16px;
+		margin-right: 16px;
+		}
+
+
+	.center{
+		text-align: center;
+	}
+	.header_detalle td{
+	padding: 1em;
+	}
+</style>
+</head>
+<body>
+	<div>
+	<table  cellspacing="0" cellspading="0" style="max-width:1024px;margin:0 auto;">
+		<tr>
+			<td colspan="4" align="center" class="n_b_l bold">
+				FORMATO DTL-005.03  <br>
+				INFORME   DE   ENSAYO
+			</td>
+		</tr>
+		<tr>
+			<td colspan="4" align="center" class="n_b_b b_r bold">
+				CERTIPETRO <br>
+				CENTRO DE CERTIFICACIÓN, INSPECCIÓN Y ENSAYOS DE PRODUCTOS PETROLEROS, GAS NATURAL Y DERIVADOS <br>
+				LABORATORIO DE ENSAYO ACREDITADO POR EL ORGANISMO PERUANO DE ACREDITACIÓN INDECOPI-SNA <br>
+				CON REGISTRO N° LE-033
+
+			</td>
+		</tr>
+		<tr>
+			<td colspan="4" align="center" class="n_b_b b_r bold">
+				<br>
+				INFORME   DE   ENSAYOS   N° <span class="orden_d"></span>
+			</td>
+		</tr>
+		<tr>
+			<td class="" colspan="3"></td>
+			<td class="n_b_l b_r align-r" colspan="1">I.E. N° …./ Página 1 de 1 <br>
+
+		</tr>
+		<tr class="row_title" >
+			<td  class=" bold" style="width:10px;">
+				1.
+			</td>
+			<td colspan="3" class="n_b_l b_r bold">
+				INFORMACIÓN GENERAL.
+			</td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.1</td>
+			<td class="n_b_b">Solicitante (Nombre Legal)<span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.2</td>
+			<td class="n_b_b">Dirección Legal <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.3</td>
+			<td class="n_b_b">Provincia / Departamento <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.4</td>
+			<td class="n_b_b">Teléfonos / Fax <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.5</td>
+			<td class="n_b_b">Registro Único del Contribuyente (Nº) <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.6</td>
+			<td class="n_b_b">Solicitud para Servicios de Ensayos (Nº) <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.7</td>
+			<td class="n_b_b">Fecha de Recepción de Solicitud <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.8</td>
+			<td class="n_b_b">Ensayos / Servicios Solicitados      <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">1.9</td>
+			<td class="n_b_b">Fecha de Inicio de Ensayo <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="">1.10</td>
+			<td class="">Fecha de Término de Ensayo <span class="orden_d"> </span></td>
+			<td class=" b_r" colspan="2"></td>
+		</tr>
+		
+		<tr class="row_title" >
+			<td  class=" bold">
+				2.
+			</td>
+			<td colspan="3" class="n_b_l b_r bold">
+				INFORMACIÓN DEL PRODUCTO.
+			</td>
+		</tr>
+		<tr>
+			<td class="n_b_b">2.1</td>
+			<td class="n_b_b">Producto (Nombre Genérico) <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">2.2</td>
+			<td class="n_b_b">Identificación de las Muestras Recepcionadas <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">2.3</td>
+			<td class="n_b_b">Marca Comercial o Especial de las Muestras <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">2.4</td>
+			<td class="n_b_b">Número de Muestras Recepcionadas <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="n_b_b">2.5</td>
+			<td class="n_b_b">Cantidad de Muestra Recepcionada <span class="orden_d"> </span></td>
+			<td class="n_b_b b_r" colspan="2"></td>
+		</tr>
+		<tr>
+			<td class="">2.6</td>
+			<td class="">Forma de Presentación de las Muestras <span class="orden_d"> </span></td>
+			<td class=" b_r" colspan="2"></td>
+		</tr>
+		<tr class="row_title" >
+			<td  class=" bold">
+				3.
+			</td>
+			<td colspan="3" class="n_b_l b_r bold">
+				RESULTADOS.
+			</td>
+		</tr>
+		<tr class="header_detalle">
+			<td  class="bold center">Item</td>
+			<td  class="bold center">ENSAYOS</td>
+			<td  class="bold center " style="width: 135px;" colspan="">MÉTODO  DE  ENSAYO</td>
+			<td  class="bold center b_r" style="width: 135px;" colspan="">RESULTADOS OBTENIDOS</td>
+			
+		</tr>
+		<tr class="">
+			<td  class="bold center">3.1</td>
+			<td  class="bold center"></td>
+			<td  class="bold center " style="width: 135px;" colspan=""></td>
+			<td  class="bold center b_r" style="width: 135px;" colspan=""></td>
+			
+		</tr>
+
+		<tr class="row_title" >
+			<td  class=" bold">
+				4.
+			</td>
+			<td colspan="3" class="n_b_l b_r bold">
+				OBSERVACIONES.
+			</td>
+		</tr>
+		<tr>
+			<td colspan="4 " class="b_r" height="25px"></td>
+
+		</tr>
+		<tr>
+			<td colspan="4" class="n_b_b b_r" style="paddin-right:40px;">
+				Especificaciones sobre el Uso del Informe de Ensayos : <br>
+-   El presente informe de ensayos se refiere únicamente a la muestra analizada. <br>
+-   Cualquier corrección o enmienda en el contenido del presente documento,  lo anula automáticamente. <br>
+-  Este informe de ensayo es un documento oficial de interés público, su adulteración o uso indebido constituye delito contra la fe pública y se regula por las disposiciones penales y civiles en la materia. Sin perjuicio de lo señalado, dicho uso puede configurar por sus efectos una infracción a las  normas de protección al consumidor y las que regulan la libre competencia. <br>
+-  Esta prohibida la reproducción parcial del presente informe de ensayo. El uso de la reproducción parcial también constituye un delito contra la fe pública. <br>
+-  El presente informe de ensayos es válido por noventa (90) días calendarios, contados a partir de la fecha de su emisión. <br>
+-  El presente informe de ensayos, el logotipo y  nombre de CERTIPETRO no pueden ser utilizados para fines publicitarios. Salvo previa autorización escrita 
+    del Director Ejecutivo de CERTIPETRO o del Decano  de la Facultad de Ingeniería de Petróleo, Gas Natural y Petroquímica - UNI. <br>
+-  Los resultados de ensayos del presente  Informe de Ensayos no deben ser utilizados como una certificación de conformidad con normas de producto o como certificado del sistema de calidad de la entidad que lo produce. <br>
+Rimac, ..... de ..... del ...
+			</td>
+			
+		</tr>
+		<tr class="firma">
+		
+		<td class="center b_r " colspan="4" cellspadding="16px">
+			
+<div class="firma center" style="width:400px;margin:0;display: inline-block;">
+	<p class="bold"><hr></p>			
+	<p class="bold">Nombre del Director </p>
+	<p class="bold">(Firma)</p> 
+	<p class="bold" style="">DIRECTOR TÉCNICO DEL CERTIPETRO</p>
+	<p class="bold" style="">(Firma y Sello)</p> </div>
+
+		</td>
+		
+	</tr>
+	<tr>
+		<td colspan="4">Formato DTL-005.03</td>
+	</tr>
+	
+	</table>
+</div>
+</body>
+</html>
